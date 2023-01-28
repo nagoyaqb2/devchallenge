d72f5a491bd6f8a0b2448cd8fe3f41cef427c196
Gábor Nagy <gabor.nagy@gyapinet.hu>
2023-01-29 00:12:18 +0100

    Home changes, typeface included

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,40 +1,106 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+import { Toaster } from "react-hot-toast";
+import { toast } from "react-hot-toast";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import AddModal from "@/components/AddModal/AddModal";
+import EditModal from "@/components/EditModal/EditModal";
+
+import { glysa } from "./layout";
+import { lexend } from "./layout";
 
 type Contact = {
   id: string;
   name: string;
   email: string;
   phone: string;
-  image: string;
+  imageUrl: string;
 };
 
 const Home = () => {
-  const fetchContacts = async (): Promise<Contact[]> => {
-    const res = await fetch("/api/get-contacts");
-    return res.json();
+  const router = useRouter();
+  const [addModalOpen, setAddModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  // Fetch contacts function
+  const fetchContacts = async () => {
+    const res = await axios.get<Contact[]>(`/api/get-contacts`);
+    return res.data;
   };
 
+  // Fetch contacts query
   const { data, isLoading, error } = useQuery({
     queryKey: ["contacts"],
     queryFn: fetchContacts,
   });
 
-  if (isLoading) return <div>Loading...</div>;
-
+  // Error handling
   if (error) return <div>Something went wrong</div>;
 
+  // Handle modal close
+  const handleAddModalClose = () => {
+    setAddModalOpen(false);
+  };
+
+  const handleEditModalClose = () => {
+    setEditModalOpen(false);
+  };
+
+  const deleteContact = async (id: number) => {
+    const res = await axios.post<number>(`/api/delete-contact`, {
+      id,
+    });
+
+    if (res.status === 200) {
+      toast.success("Successfully deleted!");
+      router.refresh();
+    } else {
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
-    <div>
-      <h1>JOOOOO</h1>
-      {data?.map((contact: Contact) => (
-        <div key={contact.id}>
-          <h2>{contact.name}</h2>
-          <p>{contact.email}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Toaster />
+      <div className={lexend.className}>
+        <h1>JOOOOO</h1>
+        <button onClick={() => setAddModalOpen(true)}>Add new</button>
+        <AddModal
+          handleAddModalClose={handleAddModalClose}
+          isOpen={addModalOpen}
+        />
+        {!isLoading &&
+          data?.map((contact: Contact) => (
+            <div key={contact.id}>
+              <Image
+                src={contact.imageUrl}
+                alt="Picture"
+                width={50}
+                height={50}
+              />
+              <h2>{contact.name}</h2>
+              <p>{contact.email}</p>
+              <p>{contact.imageUrl}</p>
+              <button onClick={() => setEditModalOpen(true)}>Edit</button>
+              <EditModal
+                handleEditModalClose={handleEditModalClose}
+                isOpen={editModalOpen}
+                id={parseInt(contact.id)}
+              />
+              <button onClick={() => deleteContact(parseInt(contact.id))}>
+                Delete
+              </button>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,20 @@
 import "./globals.css";
 import { Providers } from "./providers";
 
+import Glysa from "@next/font/local";
+import Lexend_Deca from "@next/font/local";
+
+export const glysa = Glysa({
+  src: "../public/fonts/Glysa.otf",
+  preload: true,
+  display: "swap",
+});
+export const lexend = Lexend_Deca({
+  src: "../public/fonts/LexendDeca-VariableFont_wght.ttf",
+  preload: true,
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: React.PropsWithChildren<unknown>) {
