d67abcfdaff79a1081094a37d5b95ef9bdf90cb3
Gábor Nagy <gabor.nagy@gyapinet.hu>
2023-01-29 00:11:10 +0100

    cloudinary pattern included

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/df4tacw8n/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
